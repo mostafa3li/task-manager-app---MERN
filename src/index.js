de1979cbad7309237dde60553930288e6741708c
Mostafa <mostapha.a.aly@gmail.com>
2019-06-07 17:18:40 +0200

    Integrating with the React client App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("./db/mongoose");
+const path = require("path");
+
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
@@ -17,6 +19,15 @@ app.use(userRouter);
 //! task routes
 app.use(taskRouter);
 //*=======================================
+
+// SERVE static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
